005d427b26d465655d70f3bfa2f2e73b7258a3a4
NINA <ninarosa.dev@pm.me>
2026-09-01 13:00:14 -0300

    Delete exercicio02.py

--- exercicio02.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que leia um inteiro, calcule e imprima o quadrado no número lido.
-
-value = int (input("Informe um numero inteiro: "))
-square = 2**value
-print(f"O quadrado de {value} é {square}")
