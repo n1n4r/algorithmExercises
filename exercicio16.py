bdada1e53cbf6ce515fd9a35db5adf461984082e
NINA <ninarosa.dev@pm.me>
2026-09-01 13:02:42 -0300

    Delete exercicio16.py

--- exercicio16.py
+++
@@ -1,6 +0,0 @@
-#Escreva um programa leia uma quantidade de tempo dada em horas, minutos e segundos e converta para um número equivalente em segundos. 
-time = input("Informe as horas no formato 24h (HH:MM:SS): ")
-hour = int(time[0:2])
-minutes = int(time[3:5])
-seconds = int(time[6:]) 
-print(f"O tempo {time} possui {((hour*60*60)+(minutes*60)+(seconds))} segundos")
