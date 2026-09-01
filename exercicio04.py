c8dc8d0efce51310d4576aab080010da73df954e
NINA <ninarosa.dev@pm.me>
2026-09-01 13:00:47 -0300

    Delete exercicio04.py

--- exercicio04.py
+++
@@ -1,6 +0,0 @@
-#Escreva  um  programa  que  leia  dois  números  de  ponto  flutuante  e  imprima  a  soma  desses números. 
-
-value1 = float(input("Informe o primeiro valor: "))
-value2 = float(input("Informe o segundo valor: "))
-sum = value1 + value2
-print(f"A soma de {value1} e {value2} é {sum}")
