7b451419ff5cb518033c6235c1bf84859ad31c5a
NINA <ninarosa.dev@pm.me>
2026-09-01 13:01:14 -0300

    Delete exercicio07.py

--- exercicio07.py
+++
@@ -1,6 +0,0 @@
-#Escreva um programa que leia um valor que representa o dia de hoje, outro que representa o mês e outro que representa o ano e imprima a data na tela no formato “dia/mês/ano”.
-
-day = int(input("Informe o dia: "))
-month = int(input("Informe o mês: "))
-year = int(input("Informe o ano: "))
-print(f"A data informada é {day}/{month}/{year}")
