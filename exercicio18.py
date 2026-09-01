c777ee0ab774a25d7956ecdb81cdb53b076a129c
NINA <ninarosa.dev@pm.me>
2026-09-01 13:03:00 -0300

    Delete exercicio18.py

--- exercicio18.py
+++
@@ -1,1 +0,0 @@
-#Uma companhia telefônica opera com a seguinte tarifa: uma chamada telefônica com duração de 3 minutos custa R$ 1,15. Cada minuto adicional custa R$ 0,26. Escreva um programa que leia a duração total de uma chamada (em minutos) e calcule o total a ser pago.
