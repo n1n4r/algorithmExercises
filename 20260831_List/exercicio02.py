bcd4fe27ea1c94c38f25781d05f36ac47fd76aae
NINA <ninarosa.dev@pm.me>
2026-09-01 12:58:31 -0300

    Add files

---
+++ 20260831_List/exercicio02.py
@@ -0,0 +1,5 @@
+#Escreva um programa que leia um inteiro, calcule e imprima o quadrado no número lido.
+
+value = int (input("Informe um numero inteiro: "))
+square = 2**value
+print(f"O quadrado de {value} é {square}")
