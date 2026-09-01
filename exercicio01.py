af39134eb6726db6782e70bdbe54115c72946347
NINA <ninarosa.dev@pm.me>
2026-09-01 13:00:03 -0300

    Delete exercicio01.py

--- exercicio01.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que lê um determinado valor positivo, calcula e imprime seu dobro.
-
-value = int(input("Informe um valor positivo: "))
-calc = value * 2
-print (f"O dobro do valor é: {calc}")
