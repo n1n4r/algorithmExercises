2b9fcdfc100e594f9fba10001af8ca4c97fb8c64
NINA <ninarosa.dev@pm.me>
2026-09-01 13:00:38 -0300

    Delete exercicio03.py

--- exercicio03.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que leia um número inteiro e mostre seu sucessor. 
-
-value = int(input("Informe um valor inteiro: "))
-calc = value + 1
-print (f"O sucessor de {value} é {calc}")
