9e5add45c84047d47db03b0b4505f334ed3ce836
NINA <ninarosa.dev@pm.me>
2026-09-01 13:02:07 -0300

    Delete exercicio12.py

--- exercicio12.py
+++
@@ -1,7 +0,0 @@
-#Escreva  um  programa  que  a  partir  de  um  diâmetro  de  um  círculo  que  será  digitado  pelo usuário, calcular e exibir sua área. Area = pi().raio²
-
-from math import pi
-diameter = float(input("Informe o diâmetro do círculo em centímetros: "))
-radius = diameter/2
-area = pi*(radius**2)
-print(f"A área do círculo de diâmetro {diameter:.2f}cm é {area:.2f}cm²")
