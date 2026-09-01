4bd3cbb514dbf59f69bf6dabc51ec3c44e253070
NINA <ninarosa.dev@pm.me>
2026-09-01 13:02:17 -0300

    Delete exercicio13.py

--- exercicio13.py
+++
@@ -1,6 +0,0 @@
-#Escreva um programa para calcular e exibir a área de um quadrado a partir do valor de sua diagonal que será informado pelo usuário.
-
-diagonal = float(input("Informe a diagonal do quadrado em centímetros: "))
-# diagonal² = 2 side² => diagonal = side x (2^(1/2)) => side = diagonal / (2^(1/2))
-area = (diagonal/(2**(1/2)))**2
-print(f"A área do quadrado de diagonal {diagonal:.2f}cm é {area:.2f}cm²")
