18a97e43bbc53394fa5e449cffa5dd440c214ed4
NINA <ninarosa.dev@pm.me>
2026-09-01 13:01:05 -0300

    Delete exercicio06.py

--- exercicio06.py
+++
@@ -1,7 +0,0 @@
-#Escreva um programa que leia três números de ponto flutuante e imprima a média aritmética entre eles.
-
-value1 = float(input("Informe o primeiro valor :"))
-value2 = float(input("Informe o segundo valor: "))
-value3 = float(input("Informe o terceiro valor: "))
-average = (value1 + value2 + value3)/3
-print(f"A média de {value1}, {value2} e {value3} é {average}")
