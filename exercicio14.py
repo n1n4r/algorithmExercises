7df3f84e1d2f9062bf039c139aadc2888b05e60a
NINA <ninarosa.dev@pm.me>
2026-09-01 13:02:25 -0300

    Delete exercicio14.py

--- exercicio14.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa para calcular e exibir a tensão V depois que o usuário inserir o valor da resistência R e da corrente i. V = R . i
-
-resistance = float(input("Informe o valor da resistência em ohm (Ω): "))
-current = float(input("Informe o valor da corrente em ampere (A): "))
-print(f"Para uma corrente de {current:.2f}A e resistência {resistance:.2f}Ω a tensão resultante é {(current*resistance):.2f}V")
