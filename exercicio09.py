fe3ec4b35e77d64422fb867e6181933bc006aa74
NINA <ninarosa.dev@pm.me>
2026-09-01 13:01:31 -0300

    Delete exercicio09.py

--- exercicio09.py
+++
@@ -1,6 +0,0 @@
-#Escreva um programa para calcular e exibir o volume de um cone a partir dos valores da altura e do raio da base que serão digitados pelo usuário. Volume = (1/3).pi().raio².altura 
-
-radius = int(input("Informe o raio: "))
-height = int(input("Informe a altura: "))
-volume = (1/3) * 3.14 * (radius ** 2) * height
-print(f"O volume do cone é {volume}")
